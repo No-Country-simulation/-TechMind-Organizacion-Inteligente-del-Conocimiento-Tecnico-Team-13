6b562d2510617cf4afc549a3d7ac2078f1d65f34
SamySierraDV <SamySierraDV@users.noreply.github.com>
2026-07-20 09:37:19 -0500

    Add files via upload

---
+++ hackaton/logicore/src/test/java/com/hackaton/logicore/LogicoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.logicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
